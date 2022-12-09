cbf3ff34ac9396c2c7e5da20ee85d2354403b3dd
nedja <n.duale@outlook.com>
2022-12-09 15:26:50 +0100

    module4

--- module4-LijstjesEnSamenstellingen/Deel01_collections/boodschappenlijstje.py
+++ module4-LijstjesEnSamenstellingen/Deel01_collections/boodschappenlijstje.py
@@ -1,18 +1,18 @@
-tas = {}
+boodschappentas = {}
 
 while True:
-    toevoegen= input("Wilt u een item toevoegen?")
+    toevoegen= input("Wilt u een item toevoegen aan uw booschappentas?")
     if toevoegen == 'nee':
         break 
 
 
-    product = input ("Welke producten wilt u toevoegen?")
+    product = input ("Welke product wilt u toevoegen?")
     Aantal = int(input (f"Hoeveel {product} wilt u?"))
 
-    if product not in tas:
-        tas.update({product : Aantal})
+    if product not in boodschappentas:
+        boodschappentas.update({product : Aantal})
 
 print ("---[boodschappenlijst]---")
-for keys, value in tas.items():
+for keys, value in boodschappentas.items():
     print(f"{value} x {keys}")
-print("--------------------------")
+print("--------------------------") 
